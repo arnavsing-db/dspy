a233b311f4dd726cbe8e4a03bc3bcaeceb3ad15c
arnavsing-db <159176644+arnavsing-db@users.noreply.github.com>
2024-02-29 16:15:34 -0800

    Update azure_openai.py
    remove optional logging

--- dsp/modules/azure_openai.py
+++ dsp/modules/azure_openai.py
@@ -1,11 +1,11 @@
 import logging
 
-# Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    handlers=[logging.FileHandler("azure_openai_usage.log")],
-)
+# # Configure logging
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="%(message)s",
+#     handlers=[logging.FileHandler("azure_openai_usage.log")],
+# )
 
 import functools
 import json
